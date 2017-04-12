ea2a236f7b76daf2c3e8fbe0bfc9072b5e0b43ff
james ramm <james.ramm@offpeaks.com>
2017-04-12 16:47:26 +0100

    Perform ajax checkout

--- longclaw_demo/static/js/longclaw_demo.js
+++ longclaw_demo/static/js/longclaw_demo.js
@@ -1,11 +1,15 @@
+// Javascript for longclaw demo site
 
 $(document).ready(function(){
+    // When the page loads, get the number of items in the basket 
+    // to display in the nav bar
     $.get({
         url: '/api/basket/count',
         success: function(data){ setCartCount(data.quantity); },
         headers: {"X-CSRFToken": Cookies.get('csrftoken')}
     });
 
+    // Respond to 'Add To Cart' button click events
     $('#btn-add-to-cart').click(function(e){
         var variant_id = e.target.dataset.variantId;
         $.post({
@@ -16,8 +20,15 @@ $(document).ready(function(){
         });
     });
 
+    // Respond to checkout button click events
+    $('#btn-checkout').click(checkout);
+
 });
 
+/**
+ * Set the value of the basket item counter in the nav bar
+ * @param {*} count 
+ */
 function setCartCount(count) {
     if (count === 0) {
         $('#item-count').text('');    
@@ -27,6 +38,9 @@ function setCartCount(count) {
     }
 }
 
+/**
+ * Increase the nav bar item count by 1
+ */
 function increaseItemCount() {
     var count = parseInt($('#item-count').text());
     if (count > 0) {
@@ -36,4 +50,34 @@ function increaseItemCount() {
         count = 1;
     }
     setCartCount(count);
+}
+
+/**
+ * Perform a checkout and create an order.
+ * Since we are using the dummy payment gateway,
+ * we dont need to gather any details
+ */
+function checkout() {
+    var fakeData = {
+        address: {
+            shipping_name: '',
+            shipping_address_line1: '',
+            shipping_address_city: '',
+            shipping_address_zip: '',
+            shipping_address_country: '',
+            billing_name: '',
+            billing_address_line1: '',
+            billing_address_city: '',
+            billing_address_zip: '',
+            billing_address_country: ''
+        },
+        email: '',
+        shipping: 0
+    };
+    $.post({
+        url: '/api/checkout/',
+        data: fakeData,
+        success: function(){},
+        headers: {"X-CSRFToken": Cookies.get('csrftoken')}
+    });
 }
